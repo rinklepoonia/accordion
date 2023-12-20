aaf20742c552fb0910011fa583117865a6946d32
rinklepoonia <rinklepoonia889@gmail.com>
2023-12-20 14:25:09 +0530

    update

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,14 +1,21 @@
-const accordion1 = document.querySelectorAll(".accordion1")
-accordion1.forEach((e) => e.addEventListener("click", () => {
-    var hide = e.classList.contains("active");
-    accordion1.forEach((e) => {
-        e.classList.remove("active");
+// const accordion1 = document.querySelectorAll(".accordion1")
+// const accordionHead = document.querySelectorAll(".accordionHead")
+// accordionHead.forEach((e) => e.addEventListener("click", () => {
+//     var hide = e.classList.contains("active");
+//     e.forEach((e) => {
+//         e.classList.remove("active");
+//     })
+//     // if (!hide) {
+//     //     e.classList.toggle("active");
+//     // }
+// }))
+
+
+
+
+const accordionHead = document.querySelectorAll(".accordionHead");
+accordionHead.forEach((e) => {
+    e.addEventListener("click", () => {
+        e.parentElement.classList.toggle("active")
     })
-    if (!hide) {
-        e.classList.toggle("active");
-    }
-}))
-
-
-
-
+})
